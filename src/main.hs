135f8a85326bde8ceeaf76d448e211dd0c500535
TheCraxo <62883984+TheCraxo@users.noreply.github.com>
2021-10-28 22:44:17 +0200

    Socket handle

--- src/main.hs
+++ src/main.hs
@@ -3,20 +3,40 @@ module Main where
 import System.IO
 import qualified Network.Socket as N
 
+-- Configuration options
 myServer = "irc.freenode.org" :: String
 myPort   = 6667 :: N.PortNumber
+myChan   = "#kiwiirc" :: String
+myNick   = "lopdan" :: String
 
-main :: IO ()
-main = do
-    h <- connectTo myServer myPort
-    t <- hGetContents h
-    hSetBuffering stdout NoBuffering
-    print t
-
--- Connect to a server --
+-- Connect to a server given its name and port number
 connectTo :: N.HostName -> N.PortNumber -> IO Handle
 connectTo host port = do
     addr : _ <- N.getAddrInfo Nothing (Just host) (Just (show port))
     sock <- N.socket (N.addrFamily addr) (N.addrSocketType addr) (N.addrProtocol addr)
     N.connect sock (N.addrAddress addr)
     N.socketToHandle sock ReadWriteMode
+
+-- Write to socket
+write :: Handle -> String -> String -> IO ()
+write h cmd args = do
+    let msg = cmd ++ " " ++ args ++ "\r\n"
+    hPutStr h msg
+    putStr ("> " ++ msg)
+
+--  Listen to socket
+listen :: Handle -> IO ()
+listen h = forever $ do
+    line <- hGetLine h
+    putStrLn line
+  where
+    forever :: IO () -> IO ()
+    forever a = do a; forever a
+
+-- Main function
+main :: IO ()
+main = do
+    h <- connectTo myServer myPort
+    t <- hGetContents h
+    hSetBuffering stdout NoBuffering
+    print t
